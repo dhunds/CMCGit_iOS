825ba30d91fe6254902119276397d0dc78b1cdf8
Rohit Dhundele <rohit.dhundele@gmail.com>
2015-08-19 11:48:21 +0530

    Added LoginViewController

---
+++ ClubMyCab/ClubMyCab/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ClubMyCab
+//
+//  Created by Rohit Dhundele on 19/08/15.
+//  Copyright (c) 2015 ClubMyCab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
